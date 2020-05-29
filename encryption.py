35afef196162116c9677fd431f2cd41dfc394332
Emman Durante <emmanuelii_durante@trendmicro.com>
2020-05-29 12:59:15 +0800

    Move encryption into one file

--- encryption.py
+++ encryption.py
@@ -8,15 +8,10 @@ from Crypto.Random import get_random_bytes
 # https://passwordsgenerator.net/
 SHARED_KEY = 'K3QgSHgHcs7hetbdGsuEBqtsGdNpt8pP7dAWSXnuZ9L6TSWYzQKWmMmzgK4ApKggHEDXYhazs9gLLXUQk4wy2m57kpdNLrs4AqZWYv96De55kqCUwAc4ZgBWbyVY5uqQ'
 
-# We will use file name of the file we want to encrypt
-# as the `message` part for creating the hash key
+# We will use file name of the file we want to encrypt as the `message` part for creating the hash key
 FILENAME = 'ENCRYPT_ME'
 
-
-def encrypt_content():
-    # Read the file we want to encrypt
-    file = open(f'{FILENAME}.txt', 'r')
-    file_contents = file.read()
+def encrypt_content(raw_content):
 
     # iv = initialization vector for semantic security
     iv = get_random_bytes(16)
@@ -33,4 +28,24 @@ def encrypt_content():
 
     # Encrypt our message using the cipher instance
     # NOTE: we prepend 'iv' so we can recreate our cipher instance when we will decrypt it (see decryption part)
-    return iv + cipher.encrypt(str.encode(file_contents))
+    return iv + cipher.encrypt(str.encode(raw_content))
+
+def decrypt_content(encrypted_content):
+
+    # Get the first 16 bytes (this will be used for recreating our cipher instance)
+    iv = encrypted_content[:16]
+
+    # This is the raw encrypted data
+    encrypted_data = encrypted_content[16:]
+
+    # Generating our hash key using filename and shared key
+    hmac_token = hmac.new(
+        SHARED_KEY.encode(),
+        FILENAME.encode(),
+        hashlib.md5
+    ).hexdigest()
+
+    # Create our cipher instance using the 'initialization vector' and 'hash key'
+    cipher = AES.new(hmac_token.encode(), AES.MODE_CFB, iv)
+
+    return cipher.decrypt(encrypted_data)
--- run_me.py
+++ run_me.py
@@ -1,8 +1,11 @@
-from encryption import encrypt_content
-from decryption import decrypt_content
+from encryption import encrypt_content, decrypt_content
+
+# Read the file we want to encrypt
+file = open('ENCRYPT_ME.txt', 'r')
+raw_content = file.read()
 
 print('> Encrypting...')
-encrypted_content = encrypt_content()
+encrypted_content = encrypt_content(raw_content)
 
 print('> Encrypted file content:')
 print(encrypted_content)
